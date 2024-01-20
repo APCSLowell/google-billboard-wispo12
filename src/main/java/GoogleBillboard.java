3134a7aacf8ba84964b318237f096f0796ff58fd
wispo12 <143028172+wispo12@users.noreply.github.com>
2024-01-19 20:36:57 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -2,13 +2,59 @@
 
 public class GoogleBillboard {
 	public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
+	public void setup()
+		{
+		for(int i=2;i<e.length()-10;i++)
+			{
+			String hi=e.substring(i,i+10);
+			double num1=Double.parseDouble(num1);
+			if(isPrime(num1))
+				{
+				break
+				}
+			i=i+1;
+			}
+			ArrayList<Double> arr=new Arraylist<Double>();
+			for(int i = 2; i < e.length() - 9; i++) {
+ 				String num = e.substring(i, i+10);
+				 double sum = 0;
+						
+			for(int n = 0; n < num.length(); n++) {
+ 				double c = Double.parseDouble(num.substring(n, n+1));
+				 sum += c;
+ 			 }
+			if(sum==49)
+				{
+				double d=Double.parseDouble(num);
+				arr.add(b);
+				}
+			}
+			for(int p=0;p<5;i++)
+				{
+				System.out.println(arr.get(i));
+				}
+				
+		}
+	
 	public static void main(final String[] args) {
 		System.out.println("Replace this string with your answer!");
 	}
 	
 
 	//Finish this function
-	public boolean isPrime(double d){
+	public boolean isPrime(double d)
+	{
+		if(num1<=1)
+			{
+			return false;
+		for(int k=2;k<=Math.sqrt(num1);i++)
+			{
+			if(num1%k==0)
+			{
+			return false;
+			}
+			}
+				return true;
 	}
 
 }
